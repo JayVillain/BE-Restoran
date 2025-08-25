e4cf90326873b7da9789ef517b25988badc61aac
JayVillain <saipi3825@gmail.com>
2025-08-25 03:47:08 +0000

    Aug 25, 2025, 10:47 AM

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_menu',
+        'harga',
+        'deskripsi',
+        'gambar',
+    ];
+
+    // Relasi: 1 menu bisa masuk ke banyak order_items
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
