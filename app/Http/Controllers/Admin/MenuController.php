d0a924de0a0224b9fe99974d8fca02ce914ac9d9
JayVillain <saipi3825@gmail.com>
2025-08-25 03:54:45 +0000

    Aug 25, 2025, 10:54 AM

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -3,9 +3,49 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Menu;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    //
+    public function index()
+    {
+        $menus = Menu::all();
+        return view('admin.menus.index', compact('menus'));
+    }
+
+    public function create()
+    {
+        return view('admin.menus.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_menu' => 'required',
+            'harga' => 'required|numeric',
+        ]);
+
+        Menu::create($request->all());
+        return redirect()->route('admin.menus.index')->with('success', 'Menu berhasil ditambahkan');
+    }
+
+    public function edit($id)
+    {
+        $menu = Menu::findOrFail($id);
+        return view('admin.menus.edit', compact('menu'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $menu = Menu::findOrFail($id);
+        $menu->update($request->all());
+        return redirect()->route('admin.menus.index')->with('success', 'Menu berhasil diperbarui');
+    }
+
+    public function destroy($id)
+    {
+        Menu::destroy($id);
+        return redirect()->route('admin.menus.index')->with('success', 'Menu berhasil dihapus');
+    }
 }
