97c698c872ef3f7f2896fa17db63d094316b92d6
JayVillain <saipi3825@gmail.com>
2025-08-25 03:34:53 +0000

    Aug 25, 2025, 10:34 AM

--- database/migrations/2025_08_25_030027_create_order_items_table.php
+++ database/migrations/2025_08_25_030027_create_order_items_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('menu_id')->constrained()->onDelete('cascade');
+            $table->integer('qty');
+            $table->decimal('subtotal', 10, 2);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('order_items');
