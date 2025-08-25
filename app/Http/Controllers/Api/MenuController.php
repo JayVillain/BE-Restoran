33fa650b88725aefb31a4efbd14466aa59af0fd6
JayVillain <saipi3825@gmail.com>
2025-08-25 03:53:09 +0000

    Aug 25, 2025, 10:53 AM

--- app/Http/Controllers/Api/MenuController.php
+++ app/Http/Controllers/Api/MenuController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Menu;
 
 class MenuController extends Controller
 {
-    //
+    // List semua menu (untuk customer)
+    public function index()
+    {
+        return response()->json(Menu::all());
+    }
 }
