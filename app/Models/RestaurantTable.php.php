f7885b2b1fd7b64bda34fad81ccb88cb907e4f1e
JayVillain <saipi3825@gmail.com>
2025-08-25 03:48:34 +0000

    Aug 25, 2025, 10:48 AM

--- app/Models/RestaurantTable.php.php
+++ app/Models/RestaurantTable.php.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Table extends Model
+class RestaurantTable extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'restaurant_tables'; // 🔑 wajib biar sesuai nama tabel
+
+    protected $fillable = [
+        'nomor_meja',
+        'kapasitas',
+        'status',
+    ];
+
+    // Relasi: 1 meja bisa punya banyak order
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'table_id');
+    }
 }
