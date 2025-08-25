26c01f01cb8943c4c956ea98e3dc18efbd61f06c
JayVillain <saipi3825@gmail.com>
2025-08-25 03:53:15 +0000

    Aug 25, 2025, 10:53 AM

--- app/Http/Controllers/Api/TableController.php
+++ app/Http/Controllers/Api/TableController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\RestaurantTable;
 
 class TableController extends Controller
 {
-    //
+    // List semua meja aktif
+    public function index()
+    {
+        return response()->json(RestaurantTable::where('status', true)->get());
+    }
 }
