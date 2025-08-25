c8539e34454fcfbf2630f44f75a36ef4498862f7
JayVillain <saipi3825@gmail.com>
2025-08-25 03:47:25 +0000

    Aug 25, 2025, 10:47 AM

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'menu_id',
+        'qty',
+        'subtotal',
+    ];
+
+    // Relasi: order_item milik 1 order
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    // Relasi: order_item berhubungan dengan 1 menu
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
 }
