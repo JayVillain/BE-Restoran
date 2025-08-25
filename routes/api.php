4ee6c7a25102d36fc271379d1d7d4bcec2aa3de9
JayVillain <saipi3825@gmail.com>
2025-08-25 03:50:11 +0000

    Aug 25, 2025, 10:50 AM

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\MenuController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\TableController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes (Customer)
+|--------------------------------------------------------------------------
+| Semua route di sini diakses via aplikasi mobile (Ionic).
+*/
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Route yang butuh login customer
 Route::middleware('auth:sanctum')->group(function () {
+
+    // Menu
     Route::get('/menus', [MenuController::class, 'index']);
-    Route::post('/orders', [OrderController::class, 'store']);
-    Route::get('/orders', [OrderController::class, 'myOrders']);
+
+    // Meja (untuk customer pilih meja saat order)
+    Route::get('/tables', [TableController::class, 'index']);
+
+    // Pesanan
+    Route::post('/orders', [OrderController::class, 'store']); // bikin pesanan
+    Route::get('/orders', [OrderController::class, 'myOrders']); // riwayat pesanan customer
+
 });
